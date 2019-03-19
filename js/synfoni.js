500d16f1eae17e80d873c58cec2a811c1735e0b0
Flo Kosiol <git@flokosiol.de>
2019-03-19 13:40:04 +0100

    synfoni prio + misc stylings

--- js/synfoni.js
+++ js/synfoni.js
@@ -1,9 +1,32 @@
 const comments = document.querySelectorAll('.comment');
 if (comments && comments.length) {
   const flo = 'Kosiol, Florian';
-  comments.forEach(function(comment){
-    if(comment.innerHTML.indexOf(flo) !== -1){
+  comments.forEach(function(comment) {
+    if (comment.innerHTML.indexOf(flo) !== -1) {
       comment.classList.add('me');
+    }
+  });
+}
+
+const cells = document.querySelectorAll('.table > tbody > tr td');
+if (cells && cells.length) {
+  const hot = '<strong>!!!</strong>';
+  const medium = '<strong>!!</strong>';
+  const cold = '<strong>!</strong>';
+
+  cells.forEach(function(cell) {
+    if (cell.innerHTML.indexOf(hot) !== -1) {
+      // cell.innerHTML = '🔥';
+      cell.classList.add('prio');
+      cell.classList.add('prio-hot');
+    }
+    else if (cell.innerHTML.indexOf(medium) !== -1) {
+      cell.classList.add('prio');
+      cell.classList.add('prio-medium');
+    }
+    else if (cell.innerHTML.indexOf(cold) !== -1) {
+      cell.classList.add('prio');
+      cell.classList.add('prio-cold');
     }
   });
 }
