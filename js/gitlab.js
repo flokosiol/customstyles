11291193defc1a4959c3d1d4848edd98c2f93c9d
Flo Kosiol <git@flokosiol.de>
2021-01-26 21:27:33 +0100

    fix own gitlab issues

--- js/gitlab.js
+++ js/gitlab.js
@@ -1,7 +1,7 @@
-const authorLinks = document.querySelectorAll('.issues-list .issue .author_link[href="/flo"]');
+const authorAvatars = document.querySelectorAll('.issues-list .issue .author_link[href="/flo"] .avatar');
 
-if (authorLinks && authorLinks.length > 0) {
-  authorLinks.forEach(
+if (authorAvatars && authorAvatars.length > 0) {
+  authorAvatars.forEach(
     function(element) {
       element.closest('.issue').classList.add('flo');
     }
